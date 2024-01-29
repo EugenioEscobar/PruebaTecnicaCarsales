e40a5f2ec6b1edd6c3bf286947e7d60de98b765c
EugenioEscobar <eugenio.escobar18@gmail.com>
2024-01-29 12:50:19 -0300

    creating first components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { EpisodeComponent } from './components/episode/episode.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'landing', component: LandingComponent },
+    { path: 'episode', component: EpisodeComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'landing' },];
